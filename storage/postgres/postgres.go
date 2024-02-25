3411db0cd8a42f739af7021a03358593900aca23
mentalisit <mentalisit@gmail.com>
2024-02-25 23:25:22 +0200

    5

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -35,8 +35,66 @@ func NewDb(log *logger.Logger, cfg *config.ConfigBot) *Db {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	return &Db{
+	db := &Db{
 		db:  pool,
 		log: log,
+	}
+	go db.createTable()
+	return db
+}
+func (d *Db) createTable() {
+	d.db.Exec(context.Background(), "CREATE SCHEMA IF NOT EXISTS compendium")
+	// Создание таблицы compendium.corpmember
+	_, err := d.db.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS compendium.corpmember (
+        guildid      TEXT,
+        name         TEXT,
+        userid       TEXT,
+        clientuserid TEXT,
+        avatar       TEXT,
+        tech         JSONB,
+        avatarurl    TEXT,
+        timezona     TEXT,
+        zonaoffset   NUMERIC,
+        afkfor       TEXT
+    )`)
+	if err != nil {
+		fmt.Println("Ошибка при создании таблицы compendium.corpmember:", err)
+		return
+	}
+
+	// Создание таблицы compendium.guild
+	_, err = d.db.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS compendium.guild (
+        token TEXT,
+        url   TEXT,
+        id    TEXT,
+        name  TEXT,
+        icon  TEXT
+    )`)
+	if err != nil {
+		fmt.Println("Ошибка при создании таблицы compendium.guild:", err)
+		return
+	}
+
+	// Создание таблицы compendium.identity
+	_, err = d.db.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS compendium.identity (
+        id TEXT PRIMARY KEY
+    )`)
+	if err != nil {
+		fmt.Println("Ошибка при создании таблицы compendium.identity:", err)
+		return
+	}
+
+	// Создание таблицы compendium.user
+	_, err = d.db.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS compendium."user" (
+        token         TEXT,
+        id            TEXT,
+        username      TEXT,
+        discriminator TEXT,
+        avatar        TEXT,
+        avatarurl     TEXT
+    )`)
+	if err != nil {
+		fmt.Println("Ошибка при создании таблицы compendium.user:", err)
+		return
 	}
 }
